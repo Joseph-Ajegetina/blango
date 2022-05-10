40ccb830ae56f024db6490a47a2903dc2a2cd31c
Joseph Ajongba <josephajongbah@gmail.com>
2022-05-10 12:50:06 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -51,12 +51,17 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      "django.contrib.sites",
       'django.contrib.staticfiles',
       'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
+      "allauth",
+      "allauth.account",
+      "allauth.socialaccount",
+      "allauth.socialaccount.providers.google"
       
   ]
 
@@ -164,6 +169,15 @@ class Dev(Configuration):
 
   #Server for mail_admins
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+  #Django allauth
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
+
 
   LOGGING = {
     "version": 1,
