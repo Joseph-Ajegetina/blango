523b42b29ff5cd816eac6c01188a5851e2f7f4a0
Joseph Ajongba <josephajongbah@gmail.com>
2025-03-31 14:59:23 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 class PostList(generics.ListCreateAPIView):
     authentication_classes = [SessionAuthentication]
 from blog.api.serializers import PostSerializer
@@ -15,4 +17,5 @@ class PostList(generics.ListCreateAPIView):
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
+    permissions_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     serializer_class = PostSerializer
