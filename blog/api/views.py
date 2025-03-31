a642ed3207eb11da7e8dd029f5bbada68cdff3e4
Joseph Ajongba <josephajongbah@gmail.com>
2025-03-31 17:06:41 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,6 +3,9 @@ from rest_framework.authentication import SessionAuthentication
 
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
+
 class PostList(generics.ListCreateAPIView):
     authentication_classes = [SessionAuthentication]
 from blog.api.serializers import PostSerializer
@@ -19,3 +22,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     permissions_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
